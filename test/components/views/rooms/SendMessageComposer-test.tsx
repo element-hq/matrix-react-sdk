0a8709fd9a998a8ef40c956410c40bbedd1b3388
Florian Duros <florian.duros@ormaz.fr>
2024-10-10 15:35:15 +0200

    Replace `MatrixClient.isCryptoEnabled` by `MatrixClient.getCrypto`

--- test/components/views/rooms/SendMessageComposer-test.tsx
+++ test/components/views/rooms/SendMessageComposer-test.tsx
@@ -571,7 +571,6 @@ describe("<SendMessageComposer/>", () => {
 
     it("should call prepareToEncrypt when the user is typing", async () => {
         const cli = stubClient();
-        cli.isCryptoEnabled = jest.fn().mockReturnValue(true);
         cli.isRoomEncrypted = jest.fn().mockReturnValue(true);
         cli.prepareToEncrypt = jest.fn();
         const room = mkStubRoom("!roomId:server", "Room", cli);
