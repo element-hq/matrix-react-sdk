7bc5c5873de7ba345cd616de75ae90cc45a73063
Florian Duros <florian.duros@ormaz.fr>
2024-10-16 12:35:07 +0200

    Replace `import` by `import type`

--- playwright/e2e/crypto/utils.ts
+++ playwright/e2e/crypto/utils.ts
@@ -7,10 +7,10 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { expect, JSHandle, type Page } from "@playwright/test";
-import { ICreateRoomOpts, MatrixClient } from "matrix-js-sdk/src/matrix";
-import { CryptoEvent } from "matrix-js-sdk/src/crypto-api";
 
+import type { ICreateRoomOpts, MatrixClient } from "matrix-js-sdk/src/matrix";
 import type {
+    CryptoEvent,
     EmojiMapping,
     ShowSasCallbacks,
     VerificationRequest,
